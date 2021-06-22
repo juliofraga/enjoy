d2393ca8f863f160081f240c8178e0e0fa04ddaf
juliofraga <julio_fraga10@hotmail.com>
2021-06-22 17:26:43 -0300

    Ajustes no retorno p/ o usuário

--- app/Views/viagem/finalizar.php
+++ app/Views/viagem/finalizar.php
@@ -1,3 +1,7 @@
 <div id="finalizarPost">
-	<?= $dados["mensagem"] ?>
+	<?php if($dados["resultado"] == "sucesso"){?>
+			<div class='alert alert-success' role='alert'><?= $dados["mensagem"] ?></div>
+	<?php }else{ ?>
+			<div class='alert alert-danger' role='alert'><?= $dados["mensagem"] ?></div>
+	<?php }?>
 </div>
