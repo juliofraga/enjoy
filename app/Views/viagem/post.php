fb5ed26df36d93727906560cc27ce672b674960c
juliofraga <julio_fraga10@hotmail.com>
2021-06-24 17:41:48 -0300

    Update post.php

--- app/Views/viagem/post.php
+++ app/Views/viagem/post.php
@@ -1,14 +1,14 @@
 <main style="background-color:white; width:100%;">
   <div class="container marketing">
       <!-- TIÍTULO E SUBTÍTULO -->
-      <h1 style="text-align:center; padding-top:70px;padding-bottom:80px;">NOME DO DESTINO</h1>
-      <h3 style="text-align:center; padding-top:10px;padding-bottom:80px;">BREVE DESCRIÇÃO DO DESTINO</h3>
+      <h1 style="text-align:center; padding-top:70px;padding-bottom:80px;"><?= $dados["dados"][0]->local ?></h1>
+      <h3 style="text-align:center; padding-top:10px;padding-bottom:80px;"><?= $dados["dados"][0]->datvia ?></h3>
       <!-- START THE FEATURETTES -->
   
       <div class="row featurette">
         <div class="col-md-7">
-          <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-          <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+          <h2 class="featurette-heading">Comentários sobre a viagem <h2>
+          <p class="lead"><?= $dados["dados"][0]->ovevia ?></p>
         </div>
         <div class="col-md-5">
           <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
@@ -18,8 +18,8 @@
   
       <div class="row featurette">
         <div class="col-md-7 order-md-2">
-          <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-          <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+          <h2 class="featurette-heading">Pontos positivos.</h2>
+          <p class="lead"><?= $dados["dados"][0]->ponpos ?></p>
         </div>
         <div class="col-md-5 order-md-1">
           <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
@@ -29,8 +29,8 @@
   
       <div class="row featurette">
         <div class="col-md-7">
-          <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-          <p class="lead">And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.</p>
+          <h2 class="featurette-heading">Pontos Negativos. </h2>
+          <p class="lead"><?= $dados["dados"][0]->ponneg ?></p>
         </div>
         <div class="col-md-5">
           <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
