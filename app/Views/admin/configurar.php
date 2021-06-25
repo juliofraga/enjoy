ee75f8ff6d59206950274037694dfd2fdaf73345
juliofraga <julio_fraga10@hotmail.com>
2021-06-25 16:32:48 -0300

    Update configurar.php

--- app/Views/admin/configurar.php
+++ app/Views/admin/configurar.php
@@ -10,7 +10,10 @@
 							<div class="card-body">
 								<div class="d-flex justify-content-between align-items-center">
 									<div class="btn-group">
-										<a type="button" class="btn btn-sm btn-outline-secondary" href="">Alterar Imagem</a>
+										<form method="POST" action="" enctype="multipart/form-data">
+											<input type="file" class="form-control-file" name="img1" accept="image/*">&nbsp;
+											<a type="submit" class="btn btn-sm btn-outline-secondary" href="">Alterar</a>
+										</form>
 									</div>
 								</div>
 							</div>
@@ -22,7 +25,10 @@
 							<div class="card-body">
 								<div class="d-flex justify-content-between align-items-center">
 									<div class="btn-group">
-										<a type="button" class="btn btn-sm btn-outline-secondary" href="">Alterar Imagem</a>
+										<form method="POST" action="" enctype="multipart/form-data">
+											<input type="file" class="form-control-file" name="img1" accept="image/*">&nbsp;
+											<a type="submit" class="btn btn-sm btn-outline-secondary" href="">Alterar</a>
+										</form>
 									</div>
 								</div>
 							</div>
@@ -34,7 +40,10 @@
 							<div class="card-body">
 								<div class="d-flex justify-content-between align-items-center">
 									<div class="btn-group">
-										<a type="button" class="btn btn-sm btn-outline-secondary" href="">Alterar Imagem</a>
+										<form method="POST" action="" enctype="multipart/form-data">
+											<input type="file" class="form-control-file" name="img1" accept="image/*">&nbsp;
+											<a type="submit" class="btn btn-sm btn-outline-secondary" href="">Alterar</a>
+										</form>
 									</div>
 								</div>
 							</div>
@@ -43,8 +52,23 @@
 				</div>
 			</div>
 		</div>
+		<div class="form-group row">
+			<div class="col-md-12 mt-2">
+				<input type="submit" class="btn btn-primary" style="width:100%;" value="Pré-visualizar" name="preview">
+			</div>
+		</div>
+		<div class="form-group row">
+			<div class="col-md-12 mt-2">
+				<input type="submit" class="btn btn-success" style="width:100%;" value="Aplicar Alterações" name="aplicar">
+			</div>
+		</div>
+		<div class="form-group row">
+			<div class="col-md-12 mt-2">
+				<input type="submit" class="btn btn-danger" style="width:100%;" value="Descartar Alterações" name="descartar">
+			</div>
+		</div>
 		<hr>
-		<h5>POSTS NA PÁGINA INICIAL</h5>
+		<h5 style="padding-top: 10%">POSTS NA PÁGINA INICIAL</h5>
 		<div class="album py-5 bg-light">
 			<div class="container">
 				<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
@@ -87,14 +111,29 @@
 				</div>
 			</div>
 		</div>
+		<div class="form-group row">
+			<div class="col-md-12 mt-2">
+				<input type="submit" class="btn btn-primary" style="width:100%;" value="Pré-visualizar" name="preview">
+			</div>
+		</div>
+		<div class="form-group row">
+			<div class="col-md-12 mt-2">
+				<input type="submit" class="btn btn-success" style="width:100%;" value="Aplicar Alterações" name="aplicar">
+			</div>
+		</div>
+		<div class="form-group row">
+			<div class="col-md-12 mt-2">
+				<input type="submit" class="btn btn-danger" style="width:100%;" value="Descartar Alterações" name="descartar">
+			</div>
+		</div>
 		<hr>
-		<h5>POSTS MAIS VISUALIZADOS</h5>
+		<h5 style="padding-top: 10%">POSTS MAIS VISUALIZADOS</h5>
 		<?php foreach($dados["maisVisitados"] as $posts){ ?>
 			<div class="form-group row mt-3">
 				<div class="col-md-2">
 					<label><?= $posts->local ?> - <?= $posts->numvis ?></label>
 				</div>
-				<div class="col-md-1">
+				<div class="col-md-2">
 					<a class="btn btn-success btn-sm" href="<?= URL ?>/viagem/preview/<?= $posts->slug ?>">Ver Post</a>
 				</div>
 			</div>
